7d84ceae48e588c8b785d9d868327049e024fafc
Davi Mantovani Ricci <daviricci@github.com>
2022-01-15 10:23:09 -0300

    Inicia repositório DESAFIO ATECH

---
+++ core.cpp
@@ -0,0 +1,4 @@
+#include "core.h"
+namespace core{
+
+}
---
+++ core.h
@@ -0,0 +1,7 @@
+#ifndef _ATECH_CORE_H
+#define _ATECH_CORE_H
+#include <string>
+#include <iostream>
+namespace core{
+}
+#endif //_ATECH_CORE_H
