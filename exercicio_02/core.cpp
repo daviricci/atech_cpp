3bd2405daaa38b34953eafcac64bcca962161602
Davi Mantovani Ricci <daviricci@github.com>
2022-01-15 11:35:35 -0300

    Prepara ambiente exercício 02

---
+++ exercicio_02/core.cpp
@@ -0,0 +1,10 @@
+//
+// Created by davim on 15/01/2022.
+//
+
+#include "core.h"
+namespace core {
+    void print_hello_world(){
+        std::cout<< "hello world!"<<std::endl;
+    }
+};
---
+++ exercicio_02/core.h
@@ -0,0 +1,13 @@
+//
+// Created by davim on 15/01/2022.
+//
+
+#ifndef EXERCICIO_02_CORE_H
+#define EXERCICIO_02_CORE_H
+#include <iostream>
+namespace core {
+    void print_hello_world();
+};
+
+
+#endif //EXERCICIO_02_CORE_H
