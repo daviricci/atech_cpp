0de727274705ebfb4d8fb4d0bb63c7269bd4d922
Davi Mantovani Ricci <daviricci@github.com>
2022-01-15 11:21:58 -0300

    Comenta as implementações propostas

--- exercicio_01/core.cpp
+++ exercicio_01/core.cpp
@@ -7,18 +7,29 @@ namespace core{
         std::cout << "hello world!" << std::endl;
     }
     int fibonacci_01::fib(int n) {
+        /*
+         * Aqui utiliza-se função recursiva, em que são gerados "espaços" dentro de "espaços" análogo a
+         * uma busca em profundidade. Quando a codição n<=1 é satisfeita, a função chamada dentro do último
+         * "espaço" retorna um valor e assim as demais chamadas retornam valores sucessivamente obedecendo
+         * a soma fib(n-1) + fib(n-2). o Valor final retornado é o n-ésimo termo da sequência de fibonacci.
+         */
         if (n<=1){
             return n;
         }
         return fib(n-1) + fib(n-2);
     }
     int fibonacci_02::fib(int n) {
+        //int f[] = new int[n+2]; declaração original incorreta
+        /*
+         * Nesse caso, primero são gerados todos os valores da sequência de fibonacci até o n-ésimo item,
+         * retornando-o no final
+         */
         int *f = new int(n+2);
         int i;
-        f[0] = 0;
-        f[1] = 1;
+        f[0] = 0;// o termo zero é atribuído
+        f[1] = 1;// o primeiro termo é atribuído
         for (i=2;i<=n;i++){
-            f[i] = f[i-1] + f[i-2];
+            f[i] = f[i-1] + f[i-2];// com os primeiros termos inicializados, os demais são gerados até o n-ésimo
         }
         return f[n];
     }
